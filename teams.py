ac823e70528da092408311114087112b1399b27f
Christian Hintze <hintze@kongol.de>
2025-11-30 11:33:53 +0100

    add some team abbreviations

--- teams.py
+++ teams.py
@@ -1,12 +1,16 @@
+"""The abbreviations used for other teams."""
+
 OPPONENTS = {
+    "AIB": "EHC Bad Aibling",
     "BUC": "ESV Buchloe",
     "EAS": "TSV Schongau",
     "ERCL": "ERC Lechbruck",
     "EVK": "EV Königsbrunn",
     "FFB": "EV Fürstenfeldbruck",
     "KEM": "ESC Kempten",
+    "MEM": "ECDC Memmingen",
     "PFR": "EV Pfronten",
-    "SGTBW": "SG ESV Türkheim / EV Bad Wörishofen",
+    "SGTBW": "SG Türkh. / Wörish.",
     "SGUB": "SG Ulm / Burgau",
     "SON": "ERC Sonthofen",
 }
